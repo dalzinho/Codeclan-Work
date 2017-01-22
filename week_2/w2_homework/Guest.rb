f91e9b426c95067c9ecdc66604ac0f7211a9587b
John Daly <jpdaly@gmail.com>
2017-01-22 04:04:21 +0000

    added detail to guest class, and methods for adding guests to and from rooms

--- week_2/w2_homework/Guest.rb
+++ week_2/w2_homework/Guest.rb
@@ -1,5 +1,19 @@
 class Guest
 
+  attr_reader :current_room
 
+  def initialize(name)
+    @name = name
+    @current_room = nil
+  end
+
+  def send_to_room(room)
+    room.guests << self
+    @current_room = room
+  end
+
+  def leave_room
+    @current_room.guests.delete(self)
+  end
 
 end
--- week_2/w2_homework/specs/guest.spec.rb
+++ week_2/w2_homework/specs/guest.spec.rb
@@ -2,11 +2,30 @@ require 'minitest/autorun'
 require 'minitest/rg'
 
 require_relative '../Guest'
+require_relative '../Room'
 
 class TestGuest < MiniTest::Test
 
   def setup
-    @guest = Guest.new()
+    @guest = Guest.new("Frank Zappa")
+    @room = Room.new()
+  end
+
+  def test_can_add_guest_to_room
+    @guest.send_to_room(@room)
+    assert_equal([@guest], @room.guests)
+  end
+
+  def test_adds_current_room
+    @guest.send_to_room(@room)
+    assert_equal(@room, @guest.current_room)
+  end
+
+
+  def test_guest_can_leave_room
+    @guest.send_to_room(@room)
+    @guest.leave_room
+    assert_equal([], @room.guests)
   end
 
 end
