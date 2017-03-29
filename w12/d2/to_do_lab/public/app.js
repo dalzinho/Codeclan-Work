0a0860aadb82a9d04d2167c4e07784da7708abe7
John Daly <jpdaly@gmail.com>
2017-03-29 15:43:23 +0100

    some work with apis and ajax

--- w12/d2/to_do_lab/public/app.js
+++ w12/d2/to_do_lab/public/app.js
@@ -3,16 +3,19 @@ var todos = [];
 var init = function(){
   var state = JSON.parse(localStorage.getItem('todoList')) || [];
   var list = document.querySelector('#todo-list');
-  var button = document.querySelector('button');
+  var itemButton = document.querySelector('#add-item');
+  var listButton = document.querySelector('#add-list'); 
 
-  button.onclick = handleClick;
+  itemButton.onclick = handleItemButtonClick;
+  listButton.onclick = handListItemButtonClick;
 
-  populate(list, state);
+  populateList(list, state);
 
 
 }
 
-var populate = function(list, state){
+// populate to-do-lists
+var populateList = function(list, state){
   //for each item in the state, invoke addItem
   state.forEach(function(item){
     addItem(list, item);
@@ -25,7 +28,8 @@ var addItem = function(list, item){
   list.appendChild(li);  
 }
 
-var handleClick = function(){
+// click handler
+var handleItemButtonClick = function(){
   //get the value of the input box
   var input = document.querySelector('#new-item');
   var inputValue = input.value;
@@ -39,6 +43,7 @@ var handleClick = function(){
   input.value = "";
 }
 
+// save list items to local storage
 var save = function(item){
   //save the item to localStorage
   todos.push(item);
@@ -46,7 +51,33 @@ var save = function(item){
   //NOTE You'll have to use JSON.stringify
 }
 
-window.onload = init;
+// populate list of lists
+var addDropDownItem = function(name){
+  var select = document.querySelector('#list-o-lists');
+  var option = document.createElement('option');
+  option.innerText = name;
+}
+
+var populateListDropDown = function(listOfLists){
+  listOfLists.forEach(function(list){
+    addDropDownItem(list.name);
+  });
+
+  var handListItemButtonClick = function(){
+    var input = document.querySelector('#new-list');
+    var inputValue = input.value;
+    var listOfLists = document.querySelector('#list-o-lists');
+    list = new List(input.value);
+    addDropDownItem(list.name);
+  }
+
+  var List = function(name, taskArray){
+    this.name = name;
+    this.taskArray = taskArray || [];
+  }
+
+
+  window.onload = init;
 
 //ADVANCED: create a drop-down of many to-do lists that are stored in localStorage
 //HINT: you'll have to use a different data structure (an array of objects maybe?)
